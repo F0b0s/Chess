5215fb7080ae177571f9003720e129770ff08096
юленька <@Alpha.(none)>
2014-05-04 23:39:52 +0400

    Board has been added

--- Chess/App_Start/BundleConfig.cs
+++ Chess/App_Start/BundleConfig.cs
@@ -22,7 +22,7 @@ namespace Chess
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css", "~/Content/chessboard-0.3.0.css"));
         }
     }
 }
