226c2d3c4584850ade3e9784152bcea65ec88dd2
Popov Sergey <sergppv@gmail.com>
2018-08-15 11:51:58 +0300

    Angular 2+ SPA frontend application has been added.

---
+++ src/frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import {ChessboardModule} from 'ng2-chessboard';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ChessboardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
